c825190ecc4e4de79c43126eb7f4f9549ffb5a4d
jkk290 <jared@kuromoto.dev>
2025-07-25 06:41:50 -1000

    Initial commit

---
+++ routes/usersRouter.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const usersController = require('../controllers/usersController');
+const usersRouter = Router();
+
+usersRouter.get('/', usersController.usersListGet);
+usersRouter.get('/create', usersController.usersCreateGet);
+usersRouter.get('/:id/update', usersController.usersUpdateGet);
+usersRouter.post('/create', usersController.usersCreatePost);
+usersRouter.post('/:id/update', usersController.usersUpdatePost);
+usersRouter.post('/:id/delete', usersController.usersDeletePost);
+
+module.exports = usersRouter;
